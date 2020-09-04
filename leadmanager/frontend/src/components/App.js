10e9c38dcca8186f408c121d2411630d1f64c18a
Katherine Michel <kthrnmichel@gmail.com>
2020-09-04 17:21:04 -0500

    Second milestone

--- leadmanager/frontend/src/components/App.js
+++ leadmanager/frontend/src/components/App.js
@@ -1,12 +1,23 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import ReactDOM from 'react-dom';
 
 import Header from './layout/Header';
+import Dashboard from './leads/Dashboard';
+
+import { Provider } from 'react-redux';
+import store from '../store';
 
 class App extends Component {
     render() {
         return (
-            <Header />
+            <Provider store={store}>
+                <Fragment>
+                    <Header />
+                    <div className="container">
+                        <Dashboard />
+                    </div>
+                </Fragment>
+            </Provider>
         )
     }    
 }
